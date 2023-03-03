9b2c60230bd10498daca4cc89d52c40b9730c533
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-03-03 13:44:57 +0000

    Working on the console

---
+++ console.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import cmd
+
+class HBNB(cmd.Cmd):
+	"""Console"""
+	prompt = "(hbnb) "
+
+	def do_quit(self):
+		"""Exit Console"""
+		return True
+
+	def do_EOF(self):
+		"""Exit Console"""
+		return True
+
+	def emptyline(self):
+		pass
