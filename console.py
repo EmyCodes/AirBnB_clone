d7f6cdbcebb35b4dc9f2f7b82ef2a73dfd736c16
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-03-03 23:19:37 +0000

    updated

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import cmd
 from models.base_model import BaseModel
+from models import storage
 
 class HBNBCommand(cmd.Cmd):
 	"""Console"""
@@ -44,11 +45,41 @@ class HBNBCommand(cmd.Cmd):
 			print("** class name missing **")
 		elif args[0] not in self.__classes:
 			print("** class doesn't exist **")
-		elif len(args) != 2:
+		elif len(args) == 1:
 			print("** instance id missing **")
+		elif f"{args[0]}.{args[1]}" not in storage.all():
+			print("** no instance found **")
 		else:
-			pass
+			print(storage.all()[f"{args[0]}.{args[1]}"])
 			
+	def do_destroy(self, arg):
+		""" Deletes an instance based on the class name and id (save the change into the JSON file)
+		"""
+		args = arg.split()
+
+		if len(args) == 0:
+			print("** class name missing **")
+		elif args[0] not in self.__classes:
+			print("** class doesn't exist **")
+		elif len(args) == 1:
+			print("** instance id missing **")
+		elif f"{args[0]}.{args[1]}" not in storage.all():
+			print("** no instance found **")
+		else:
+			del storage.all()[f"{args[0]}.{args[1]}"]
+			storage.save()
+		
+	def do_all(self, arg):
+		"""Prints all string representation of all instances based or not on the class name.
+		"""
+		args = arg.split()
+			
+		if len(args) == 0:
+			print([str(v) for v in storage.all().values()])
+		elif args[0] not in self.__classes:
+			print("** class doesn't exist **")
+		else:
+			print([str(v) for k, v in storage.all().items() if k.startswith(args[0])])
 
 
 if __name__ == "__main__":
