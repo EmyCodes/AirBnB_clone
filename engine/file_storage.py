80fc2f81df249450c4c94646b8920216132e881f
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-03-07 01:13:45 +0000

    engine directories

---
+++ engine/file_storage.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+import json
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+# import models
+
+class FileStorage():
+	"""Serializes to JSON file and deserializes JSON file to instances"""
+	#Private class Attributes
+	__file_path = "file.json"
+	__objects = {}
+
+	# Public Istance Methods
+	def all(self):
+		"""Returns Dictionaary Objects"""
+		return self.__objects
+
+	def new(self, obj):
+		""" """
+		key = f"{obj.__class__.__name__}.{obj.id}"
+		self.__objects[key] = obj
+	
+	def save(self):
+		with open(self.__file_path, "w", encoding="utf-8") as f:
+			d = {k: v.to_dict() for k, v in self.__objects.items()}
+			json.dump(d, f)
+	
+	def reload(self):
+		try:
+			with open(self.__file_path, "r", encoding="utf-8") as f:
+				obj_dict = json.load(f)
+				for o in obj_dict.values():
+					cls_name = o["__class__"]
+					del o["__class__"]
+					self.new(eval(f"{cls_name}")(**o))
+
+		except FileNotFoundError:
+			return
+		
